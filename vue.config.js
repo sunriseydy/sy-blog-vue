215fa61f2419ec62a02635972d49035565c679b3
SunriseYDY <me@sunriseydy.dev>
2022-03-21 17:40:34 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: [
+    'vuetify'
+  ],
+  // https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    port: 9090
+  },
+  assetsDir: 'static'
 })
