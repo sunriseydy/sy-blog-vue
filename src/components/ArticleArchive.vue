405585fedf57dc774a3352847222201a7690c31b
SunriseYDY <me@sunriseydy.dev>
2022-05-05 22:36:54 +0800

    [ADD]文章列表组件添加接口调用

--- src/components/ArticleArchive.vue
+++ src/components/ArticleArchive.vue
@@ -8,9 +8,45 @@
 </template>
 
 <script>
+import api from '@/api';
+
 export default {
-  name: 'ArticleArchive'
-}
+  name: 'ArticleArchive',
+  data: () => ({
+    page: 0,
+    pageSize: 10,
+    totalPages: 1,
+    posts: [],
+  }),
+  mounted() {
+    this.getPosts();
+  },
+  methods: {
+    getPosts(page = 0) {
+      // 获取文章
+      if (page === this.totalPages) {
+        this.$dialog.notify.info('已经最后一页了', {
+          position: 'bottom-right',
+          timeout: 3000,
+        });
+        return;
+      }
+      api.getPosts({
+        page,
+        pageSize: this.pageSize,
+      }).then(res => {
+        const {page, pageSize, totalPages, content} = res.data.data;
+        this.page = page;
+        this.pageSize = pageSize;
+        this.totalPages = totalPages;
+        this.posts = content;
+      });
+    },
+    getNextPagePosts() {
+      this.getPosts(this.page + 1);
+    },
+  },
+};
 </script>
 
 <style scoped>
