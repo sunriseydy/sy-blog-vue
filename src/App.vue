ea19b0e1ecabb8235d475f2927c843b0c9e019d2
SunriseYDY <me@sunriseydy.dev>
2022-05-12 20:35:57 +0800

    [ADD]添加跳转到顶部按钮

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,17 @@
   <v-app id="inspire">
     <app-bar />
     <app-content />
+    <fab-to-top />
   </v-app>
 </template>
 
 <script>
 import AppBar from '@/components/core/AppBar';
 import AppContent from '@/components/core/AppContent';
+import FabToTop from '@/components/FabToTop';
 
 export default {
-  components: {AppContent, AppBar},
+  components: {FabToTop, AppContent, AppBar},
 };
 
 </script>
---
+++ src/components/FabToTop.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-fab-transition>
+    <v-btn
+        v-show="fab"
+        v-scroll="onScroll"
+        bottom
+        class="transition-swing"
+        fab
+        fixed
+        large
+        right
+        style="z-index: 6"
+        @click="toTop"
+    >
+      <v-icon>mdi-chevron-up</v-icon>
+    </v-btn>
+  </v-fab-transition>
+</template>
+
+<script>
+export default {
+  name: 'FabToTop',
+  data: () => ({fab: false}),
+  methods: {
+    onScroll() {
+      this.fab = window.scrollY > 300;
+    },
+    toTop() {
+      if (this.$route.hash) {
+        this.$router.push({hash: ''});
+      }
+      this.$vuetify.goTo(0);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
