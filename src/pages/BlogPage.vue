108cffa70bf93bdbe1bf560160ab862cee174cb4
SunriseYDY <me@sunriseydy.dev>
2022-05-23 15:30:32 +0800

    [IMP]page和post如果没有目录则不展示toc的col

--- src/pages/BlogPage.vue
+++ src/pages/BlogPage.vue
@@ -2,17 +2,17 @@
   <v-row>
     <v-col
         lg="2"
-        v-if="$vuetify.breakpoint.lgAndUp"
+        v-if="$vuetify.breakpoint.lgAndUp && hasToc"
     >
       <content-catalog />
     </v-col>
     <v-col
-        lg="8"
+        :lg="hasToc ? 8 : 9"
     >
       <article-detail is-page />
     </v-col>
     <v-col
-        lg="2"
+        :lg="hasToc ? 2 : 3"
         v-if="$vuetify.breakpoint.lgAndUp"
     >
       <side-bar />
@@ -28,6 +28,11 @@ import SideBar from '@/layouts/SideBar';
 export default {
   name: 'BlogPage',
   components: {SideBar, ContentCatalog, ArticleDetail},
+  computed: {
+    hasToc() {
+      return this.$store.state.toc.length !== 0;
+    },
+  },
 }
 </script>
 
